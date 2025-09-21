054464eca3bff169a9305e2145c2cd61a2a43928
LuzkalidGM <gg2022074263@virtual.upt.pe>
2025-09-20 22:27:00 -0500

    🏗️ Fix SQL Server name conflict - Update to upt-dbs-996

--- infra/main.tf
+++ infra/main.tf
@@ -35,12 +35,12 @@ provider "azurerm" {
 ## Eliminado random_integer para usar nombres fijos
 
 resource "azurerm_resource_group" "rg" {
-  name     = "upt-arg-990"
+  name     = "upt-arg-996"
   location = "brazilsouth"
 }
 
 resource "azurerm_mssql_server" "sqlsrv" {
-  name                         = "upt-dbs-990"
+  name                         = "upt-dbs-996"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
